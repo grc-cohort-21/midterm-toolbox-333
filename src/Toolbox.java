6fd617bd36d753eef8a1795110cb02917824afa1
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-04-22 16:10:28 -0700

    added FindNthElement

--- src/Toolbox.java
+++ src/Toolbox.java
@@ -128,7 +128,13 @@ public class Toolbox {
     if (head == null || n < 0) {
       throw new IllegalArgumentException("Head cannot be null and n cannot be negative.");
     }
-    return null; 
+
+    while (head != null && n > 0) {
+      head = head.next;
+      n--;
+    }
+
+    return head; 
   }
 
   /**
