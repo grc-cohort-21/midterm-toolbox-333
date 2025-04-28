4a2630be0ee84aebc1b671f7b71923a04944a67c
elena5100 <194572363+elena5100@users.noreply.github.com>
2025-04-27 22:51:14 -0700

    implemented rotateQueueLeft() method

--- src/Toolbox.java
+++ src/Toolbox.java
@@ -176,7 +176,14 @@ public class Toolbox {
     if (head == null) {
       throw new IllegalArgumentException("Head cannot be null.");
     }
-    
+    SingleNode current = head;
+    while (current != null && current.next != null) {
+      if (current.data > current.next.data) {
+        current.next = current.next.next;
+      } else {
+        current = current.next;
+      }
+    }
   }
 
 
